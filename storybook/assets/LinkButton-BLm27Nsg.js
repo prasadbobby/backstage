8b016100a308b8ca3e950eaa2f8a851827aa144c
Rugvip <poldsberg@gmail.com>
2024-11-21 22:19:17 +0000

    Deploying to gh-pages from @ backstage/backstage@4e103eb8bf9547dfb01a7a216b905ea045004d5b 🚀

---
+++ storybook/assets/LinkButton-BLm27Nsg.js
@@ -0,0 +1,4 @@
+import{R as o}from"./index-CTjT7uj6.js";import{L as e}from"./Link-CSDd0x4P.js";import{B as r}from"./Button-C0UHdKih.js";const i=o.forwardRef((t,n)=>o.createElement(e,{ref:n,...t,color:"initial"})),a=o.forwardRef((t,n)=>o.createElement(r,{ref:n,component:i,...t}));a.__docgenInfo={description:`Thin wrapper on top of material-ui's {@link https://v4.mui.com/components/buttons/ | Button} component
+
+@public
+@remarks`,methods:[],displayName:"LinkButton"};export{a as L};

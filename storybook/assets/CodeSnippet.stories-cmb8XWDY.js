a8040220708229e78d40eb8a66e22a8749008a0f
vinzscam <vincenzos@spotify.com>
2024-11-21 11:02:55 +0000

    Deploying to gh-pages from @ backstage/backstage@4353dd41d2d29ec7fed9de9024ee538b2039e2db 🚀

---
+++ storybook/assets/CodeSnippet.stories-cmb8XWDY.js
@@ -0,0 +1,39 @@
+import{R as e}from"./index-CTjT7uj6.js";import{C as t}from"./CodeSnippet-DlrCZusE.js";import{I as o}from"./InfoCard-C7sUJY0g.js";import"./iframe-B9cDefrU.js";import"../sb-preview/runtime.js";import"./createSvgIcon-s1DURs_u.js";import"./capitalize-CDPu30qc.js";import"./defaultTheme-CzJIUhLY.js";import"./withStyles-Cy0w16fh.js";import"./hoist-non-react-statics.cjs-yZcCaz5O.js";import"./useControlled-CogIeAPD.js";import"./index-QA7F3UF1.js";import"./createSvgIcon-ErQpyllY.js";import"./isMuiElement-Cb6QZSLO.js";import"./unstable_useId-B3Hiq1YI.js";import"./objectWithoutProperties-BHGRdSdY.js";import"./classCallCheck-BNzALLS0.js";import"./inherits-Ax9O35oZ.js";import"./toConsumableArray-C7wLl_Dv.js";import"./useTheme-C1RDaILZ.js";import"./Box-4hiNbBw1.js";import"./typography-DzPlb7_1.js";import"./CopyTextButton-Dbrs41La.js";import"./useCopyToClipboard-zhb0ohJ3.js";import"./interopRequireDefault-BbjLfwJi.js";import"./useMountedState-DkESzBh4.js";import"./translation-67MxozS3.js";import"./TranslationApi-eOxINumg.js";import"./ApiRef-BSBwTmJJ.js";import"./Tooltip-B3WgSvIa.js";import"./Popper-BsN5-wvL.js";import"./Portal-DHXjgkAG.js";import"./Grow-SXlwBtiL.js";import"./utils-Bbz8jn0C.js";import"./TransitionGroupContext-BtzQ-Cv7.js";import"./IconButton-M_kcka7o.js";import"./ButtonBase-DWNP_al0.js";import"./index-Cqve-NHl.js";import"./makeStyles-DFHNDtjR.js";import"./CardActions-CVT7cQhk.js";import"./CardContent-DkZms0rz.js";import"./Paper-BK9DXOUE.js";import"./ErrorBoundary-BkFh_a6T.js";import"./ErrorPanel-PoeRKObE.js";import"./WarningPanel-DQzJNZJM.js";import"./ExpandMore-CfNkWiXY.js";import"./AccordionDetails-ByOIwY4p.js";import"./toArray-BIgjzDMT.js";import"./react-is.production.min-D0tnNtx9.js";import"./Collapse-BmjSVfa7.js";import"./Typography-DtBgvzFq.js";import"./MarkdownContent-CfEP7XkI.js";import"./index-BRV0Se7Z.js";import"./Grid-BsFNIMVV.js";import"./List-DYtqLDXv.js";import"./ListContext-DydK1sOh.js";import"./ListItem-D0kWGnse.js";import"./ListItemText-DDba65AT.js";import"./LinkButton-BLm27Nsg.js";import"./Link-CSDd0x4P.js";import"./lodash-CoGan1YB.js";import"./index-DXJrW9ju.js";import"./useAnalytics-DTrkS1Gy.js";import"./ConfigApi-DBUUc3nU.js";import"./Button-C0UHdKih.js";import"./CardHeader-zP9t5-2f.js";import"./Divider-YusUzn_w.js";import"./BottomLink-Bhk2-oNl.js";import"./ArrowForward-CXiK1IVx.js";const Fe={title:"Data Display/CodeSnippet",component:t},l={width:300},r=`const greeting = "Hello";
+const world = "World";
+
+const greet = person => greeting + " " + person + "!";
+
+greet(world);
+`,j=`const greeting: string = "Hello";
+const world: string = "World";
+
+const greet = (person: string): string => greeting + " " + person + "!";
+
+greet(world);
+`,A=`greeting = "Hello"
+world = "World"
+
+def greet(person):
+    return f"{greeting} {person}!"
+
+greet(world)
+`,a=()=>e.createElement(o,{title:"JavaScript example"},e.createElement(t,{text:"const hello = 'World';",language:"javascript"})),i=()=>e.createElement(o,{title:"JavaScript multi-line example"},e.createElement(t,{text:r,language:"javascript"})),p=()=>e.createElement(o,{title:"Show line numbers"},e.createElement(t,{text:r,language:"javascript",showLineNumbers:!0})),n=()=>e.createElement(o,{title:"Overflow"},e.createElement("div",{style:l},e.createElement(t,{text:r,language:"javascript"})),e.createElement("div",{style:l},e.createElement(t,{text:r,language:"javascript",showLineNumbers:!0}))),s=()=>e.createElement(o,{title:"Multiple languages"},e.createElement(t,{text:r,language:"javascript",showLineNumbers:!0}),e.createElement(t,{text:j,language:"typescript",showLineNumbers:!0}),e.createElement(t,{text:A,language:"python",showLineNumbers:!0})),m=()=>e.createElement(o,{title:"Copy Code"},e.createElement(t,{text:r,language:"javascript",showCopyCodeButton:!0}));a.__docgenInfo={description:"",methods:[],displayName:"Default"};i.__docgenInfo={description:"",methods:[],displayName:"MultipleLines"};p.__docgenInfo={description:"",methods:[],displayName:"LineNumbers"};n.__docgenInfo={description:"",methods:[],displayName:"Overflow"};s.__docgenInfo={description:"",methods:[],displayName:"Languages"};m.__docgenInfo={description:"",methods:[],displayName:"CopyCode"};var c,d,g;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`() => <InfoCard title="JavaScript example">
+    <CodeSnippet text="const hello = 'World';" language="javascript" />
+  </InfoCard>`,...(g=(d=a.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var u,C,S;i.parameters={...i.parameters,docs:{...(u=i.parameters)==null?void 0:u.docs,source:{originalSource:`() => <InfoCard title="JavaScript multi-line example">
+    <CodeSnippet text={JAVASCRIPT} language="javascript" />
+  </InfoCard>`,...(S=(C=i.parameters)==null?void 0:C.docs)==null?void 0:S.source}}};var f,v,I;p.parameters={...p.parameters,docs:{...(f=p.parameters)==null?void 0:f.docs,source:{originalSource:`() => <InfoCard title="Show line numbers">
+    <CodeSnippet text={JAVASCRIPT} language="javascript" showLineNumbers />
+  </InfoCard>`,...(I=(v=p.parameters)==null?void 0:v.docs)==null?void 0:I.source}}};var w,h,y;n.parameters={...n.parameters,docs:{...(w=n.parameters)==null?void 0:w.docs,source:{originalSource:`() => <InfoCard title="Overflow">
+    <div style={containerStyle}>
+      <CodeSnippet text={JAVASCRIPT} language="javascript" />
+    </div>
+    <div style={containerStyle}>
+      <CodeSnippet text={JAVASCRIPT} language="javascript" showLineNumbers />
+    </div>
+  </InfoCard>`,...(y=(h=n.parameters)==null?void 0:h.docs)==null?void 0:y.source}}};var x,N,E;s.parameters={...s.parameters,docs:{...(x=s.parameters)==null?void 0:x.docs,source:{originalSource:`() => <InfoCard title="Multiple languages">
+    <CodeSnippet text={JAVASCRIPT} language="javascript" showLineNumbers />
+    <CodeSnippet text={TYPESCRIPT} language="typescript" showLineNumbers />
+    <CodeSnippet text={PYTHON} language="python" showLineNumbers />
+  </InfoCard>`,...(E=(N=s.parameters)==null?void 0:N.docs)==null?void 0:E.source}}};var L,b,_;m.parameters={...m.parameters,docs:{...(L=m.parameters)==null?void 0:L.docs,source:{originalSource:`() => <InfoCard title="Copy Code">
+    <CodeSnippet text={JAVASCRIPT} language="javascript" showCopyCodeButton />
+  </InfoCard>`,...(_=(b=m.parameters)==null?void 0:b.docs)==null?void 0:_.source}}};const Ge=["Default","MultipleLines","LineNumbers","Overflow","Languages","CopyCode"];export{m as CopyCode,a as Default,s as Languages,p as LineNumbers,i as MultipleLines,n as Overflow,Ge as __namedExportsOrder,Fe as default};
---
+++ storybook/assets/HeaderWorldClock.stories-B-B9htzM.js
@@ -0,0 +1,45 @@
+import{R as r}from"./index-CTjT7uj6.js";import{HeaderWorldClock as s}from"./index-BEqbUDmh.js";import{H as d}from"./Header-e6yf3tAi.js";import{w as u}from"./appWrappers-DRiz_PsS.js";import"./HeaderLabel-DNe62uCf.js";import"./makeStyles-DFHNDtjR.js";import"./defaultTheme-CzJIUhLY.js";import"./Grid-BsFNIMVV.js";import"./hoist-non-react-statics.cjs-yZcCaz5O.js";import"./capitalize-CDPu30qc.js";import"./withStyles-Cy0w16fh.js";import"./Typography-DtBgvzFq.js";import"./Link-CSDd0x4P.js";import"./index-Cqve-NHl.js";import"./lodash-CoGan1YB.js";import"./index-QA7F3UF1.js";import"./index-DXJrW9ju.js";import"./ApiRef-BSBwTmJJ.js";import"./interopRequireDefault-BbjLfwJi.js";import"./createSvgIcon-s1DURs_u.js";import"./useControlled-CogIeAPD.js";import"./createSvgIcon-ErQpyllY.js";import"./isMuiElement-Cb6QZSLO.js";import"./unstable_useId-B3Hiq1YI.js";import"./useAnalytics-DTrkS1Gy.js";import"./ConfigApi-DBUUc3nU.js";import"./Helmet-DPVyO7__.js";import"./index-BRV0Se7Z.js";import"./Box-4hiNbBw1.js";import"./typography-DzPlb7_1.js";import"./Breadcrumbs-CEPS-IZ1.js";import"./react-is.production.min-D0tnNtx9.js";import"./ButtonBase-DWNP_al0.js";import"./TransitionGroupContext-BtzQ-Cv7.js";import"./Popover-sv14Y19J.js";import"./Modal-SXLhKrl2.js";import"./classCallCheck-BNzALLS0.js";import"./Portal-DHXjgkAG.js";import"./Paper-BK9DXOUE.js";import"./Grow-SXlwBtiL.js";import"./useTheme-C1RDaILZ.js";import"./utils-Bbz8jn0C.js";import"./List-DYtqLDXv.js";import"./ListContext-DydK1sOh.js";import"./ListItem-D0kWGnse.js";import"./Page-w3IkmeGv.js";import"./useMediaQuery-JlhfXw1s.js";import"./Tooltip-B3WgSvIa.js";import"./Popper-BsN5-wvL.js";import"./MockTranslationApi-Bm0rhTQl.js";import"./index-CFaqwFgm.js";import"./inherits-Ax9O35oZ.js";import"./toArray-BIgjzDMT.js";import"./TranslationApi-eOxINumg.js";import"./WebStorage-D5Fgivzj.js";import"./useAsync-CXA3qup_.js";import"./useMountedState-DkESzBh4.js";import"./componentData-CM4hSmEF.js";import"./mapValues-uGpuQnk_.js";import"./toString-bNjqhuyi.js";import"./ApiProvider-CuNQiN7Z.js";import"./CssBaseline-DDRFgeWI.js";import"./ThemeProvider-CVGcooNy.js";import"./jsx-runtime-Cw0GR0a5.js";import"./palettes-B20oCNII.js";const _o={title:"Plugins/Home/Components/HeaderWorldClock",decorators:[o=>u(r.createElement(o,null))]},e=()=>{const o=[{label:"NYC",timeZone:"America/New_York"},{label:"UTC",timeZone:"UTC"},{label:"STO",timeZone:"Europe/Stockholm"},{label:"TYO",timeZone:"Asia/Tokyo"}],i={hour:"2-digit",minute:"2-digit",hour12:!0};return r.createElement(d,{title:"Header World Clock",pageTitleOverride:"Home"},r.createElement(s,{clockConfigs:o,customTimeFormat:i}))},t=()=>{const o=[{label:"NYC",timeZone:"America/New_York"},{label:"UTC",timeZone:"UTC"},{label:"STO",timeZone:"Europe/Stockholm"},{label:"TYO",timeZone:"Asia/Tokyo"}],i={hour:"2-digit",minute:"2-digit",hour12:!1};return r.createElement(d,{title:"24hr Header World Clock",pageTitleOverride:"Home"},r.createElement(s,{clockConfigs:o,customTimeFormat:i}))};e.__docgenInfo={description:"",methods:[],displayName:"Default"};t.__docgenInfo={description:"",methods:[],displayName:"TwentyFourHourClocks"};var m,a,l;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`() => {
+  const clockConfigs: ClockConfig[] = [{
+    label: 'NYC',
+    timeZone: 'America/New_York'
+  }, {
+    label: 'UTC',
+    timeZone: 'UTC'
+  }, {
+    label: 'STO',
+    timeZone: 'Europe/Stockholm'
+  }, {
+    label: 'TYO',
+    timeZone: 'Asia/Tokyo'
+  }];
+  const timeFormat: Intl.DateTimeFormatOptions = {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true
+  };
+  return <Header title="Header World Clock" pageTitleOverride="Home">
+      <HeaderWorldClock clockConfigs={clockConfigs} customTimeFormat={timeFormat} />
+    </Header>;
+}`,...(l=(a=e.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};var p,c,n;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`() => {
+  const clockConfigs: ClockConfig[] = [{
+    label: 'NYC',
+    timeZone: 'America/New_York'
+  }, {
+    label: 'UTC',
+    timeZone: 'UTC'
+  }, {
+    label: 'STO',
+    timeZone: 'Europe/Stockholm'
+  }, {
+    label: 'TYO',
+    timeZone: 'Asia/Tokyo'
+  }];
+  const timeFormat: Intl.DateTimeFormatOptions = {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
+  };
+  return <Header title="24hr Header World Clock" pageTitleOverride="Home">
+      <HeaderWorldClock clockConfigs={clockConfigs} customTimeFormat={timeFormat} />
+    </Header>;
+}`,...(n=(c=t.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};const Eo=["Default","TwentyFourHourClocks"];export{e as Default,t as TwentyFourHourClocks,Eo as __namedExportsOrder,_o as default};

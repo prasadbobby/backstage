62ef65cc370f79a9a7f65be516767dbc2011e3b4
aramissennyeydd <aramis.sennyey@doordash.com>
2024-12-12 21:54:22 -0700

    feat: add a new system metadata service
    Signed-off-by: aramissennyeydd <aramis.sennyey@doordash.com>

--- packages/backend-split/src/experimental/instanceMetadata.ts
+++ packages/backend-split/src/experimental/instanceMetadata.ts
@@ -18,6 +18,7 @@ import {
   createBackendPlugin,
 } from '@backstage/backend-plugin-api';
 import { instanceMetadataServiceRef } from '@backstage/backend-plugin-api/alpha';
+import Router from 'express-promise-router';
 
 // Example usage of the instance metadata service to log the installed features.
 export default createBackendPlugin({
@@ -27,11 +28,22 @@ export default createBackendPlugin({
       deps: {
         instanceMetadata: instanceMetadataServiceRef,
         logger: coreServices.logger,
+        httpRouter: coreServices.rootHttpRouter,
       },
-      async init({ instanceMetadata, logger }) {
+      async init({ instanceMetadata, logger, httpRouter }) {
         logger.info(
-          `Installed features on this instance: ${instanceMetadata.getInstalledFeatures()}`,
+          `Installed features on this instance: ${JSON.stringify(
+            instanceMetadata.getInstalledFeatures(),
+          )}`,
         );
+
+        const router = Router();
+
+        router.get('/features/installed', (_, res) => {
+          res.json({ items: instanceMetadata.getInstalledFeatures() });
+        });
+
+        httpRouter.use('/.backstage/instanceInfo', router);
       },
     });
   },
